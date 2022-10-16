ca2876d6078bd352af53f91a6c675b44cfc3666d
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2022-10-15 17:25:05 -0700

    Added

--- javaclientgen.ps1
+++ javaclientgen.ps1
@@ -3,18 +3,18 @@ Remove-Item –path ./gradle –recurse
 Remove-Item –path ./scripts –recurse
 Remove-Item –path ./src –recurse
 $artifact_version = "4.2.2"
-& java -jar openapi-generator-cli-6.0.0.jar generate   -i https://api.cloudmersive.com/validate/docs/v1/swagger  -l java -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/nlp-v2/docs/v1/swagger  -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate  -i https://api.cloudmersive.com/ocr/docs/v1/swagger  -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate   -i https://api.cloudmersive.com/barcode/docs/v1/swagger  -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate  -i https://api.cloudmersive.com/image/docs/v1/swagger  -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/convert/docs/v1/swagger -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/virus/docs/v1/swagger -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/config/docs/v1/swagger -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/video/docs/v1/swagger -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/swagger/api/dataintegration -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/swagger/api/currency -l java  -c packageconfig.json         -o .
-& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/swagger/api/security -l java  -c packageconfig.json         -o .
+& java -jar openapi-generator-cli-6.0.0.jar generate   -i https://api.cloudmersive.com/validate/docs/v1/swagger  -l java -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/nlp-v2/docs/v1/swagger  -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate  -i https://api.cloudmersive.com/ocr/docs/v1/swagger  -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate   -i https://api.cloudmersive.com/barcode/docs/v1/swagger  -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate  -i https://api.cloudmersive.com/image/docs/v1/swagger  -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/convert/docs/v1/swagger -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/virus/docs/v1/swagger -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/config/docs/v1/swagger -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/video/docs/v1/swagger -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/swagger/api/dataintegration -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/swagger/api/currency -l java  -c packageconfig.json         -o . --skip-validate-spec
+& java -jar openapi-generator-cli-6.0.0.jar generate    -i https://api.cloudmersive.com/swagger/api/security -l java  -c packageconfig.json         -o . --skip-validate-spec
 
 # Fix Android bug
 
